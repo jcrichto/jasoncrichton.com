847ebc72a735e7b003cc83a084d97886a8193da3
Jason Crichton <jcrichto@gmail.com>
2023-08-02 22:01:13 -0500

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    css: {
-        preprocessorOptions: {
-            scss: {
-                includePaths: ['src/styles/sass'], // Set the path to your Sass files directory
-            }
-        }
-    },
     vite: {
         ssr: {
             external: ["svgo"],
